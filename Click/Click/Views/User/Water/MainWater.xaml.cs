6fe0315c7bba3c4e7c47b5d218ef3ddf2a4f373a
EfimPetrov <79350523+EfimPetrov@users.noreply.github.com>
2021-02-20 15:39:27 +0900

    добавил страницу

---
+++ Click/Click/Views/User/Water/MainWater.xaml.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace Click.Views.User.Water
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class MainWater : ContentPage
+    {
+        public MainWater()
+        {
+            InitializeComponent();
+        }
+
+        private void Back_Clicked(object sender, EventArgs e)
+        {
+            Navigation.PopModalAsync();
+        }
+
+        private void Bonus_Clicked(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Bootle_Clicked(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Vodovoz_Clicked(object sender, EventArgs e)
+        {
+            //Navigation.PushModalAsync(new MainFood());
+        }
+    }
+}
